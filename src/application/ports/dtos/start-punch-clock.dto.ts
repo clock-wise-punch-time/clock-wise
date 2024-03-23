28500144752546db7d0aacbab368b6cfe789cf96
Pedro Marins <ph.marins@hotmail.com>
2024-03-23 20:38:55 -0300

    feat: implement swagger documentation

--- src/application/ports/dtos/start-punch-clock.dto.ts
+++ src/application/ports/dtos/start-punch-clock.dto.ts
@@ -1,14 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class StartPunchClockDto {
+  @ApiProperty({
+    description: 'The user id',
+  })
   @IsNumber()
   @IsNotEmpty()
   userId: number;
 
+  @ApiProperty({
+    description: 'The date',
+  })
   @IsDate()
   @IsNotEmpty()
   date: Date;
 
+  @ApiProperty({
+    description: 'The start time',
+  })
   @IsDate()
   @IsNotEmpty()
   startTime: Date;
