3d019a788d16657d20bba85929911de01c3259cb
Pedro Marins <ph.marins@hotmail.com>
2024-03-23 00:27:31 -0300

    feat: implement breaks

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,27 +5,39 @@ import {
   Module,
 } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { BreakController } from 'src/adapters/controllers/break.controller';
 import { PunchClockController } from 'src/adapters/controllers/punch-clock.controller';
 import { PrismaHelper } from 'src/adapters/database/helpers/prisma.helper';
+import { BreakRepository } from 'src/adapters/database/repositories/break.repository';
 import { PunchClockRepository } from 'src/adapters/database/repositories/punch-clock.repository';
 import { UserMiddleware } from 'src/application/middlewares/user.middleware';
+import { BreakService } from 'src/application/services/break.service';
 import { PunchClockService } from 'src/application/services/punch-clock.service';
-import { EndPunchClockUseCase } from 'src/application/usecase/end-punch-clock.usercase.ts/end-punch-clock.usecase';
+import { EndBreakUseCase } from 'src/application/usecase/end-break/end-break.usecase';
+import { EndPunchClockUseCase } from 'src/application/usecase/end-punch-clock/end-punch-clock.usecase';
 import { GetPunchClockUseCase } from 'src/application/usecase/get-punch-clock/get-punch-clock.usecase';
+import { StartBreakUseCase } from 'src/application/usecase/start-break/start-break.usecase';
 import { StartPunchClockUseCase } from 'src/application/usecase/start-punch-clock/start-punch-clock.usecase';
 
 @Module({
   imports: [HttpModule],
-  controllers: [PunchClockController],
+  controllers: [PunchClockController, BreakController],
   providers: [
     PrismaHelper,
     PunchClockService,
     StartPunchClockUseCase,
     EndPunchClockUseCase,
     GetPunchClockUseCase,
+    StartBreakUseCase,
+    EndBreakUseCase,
+    BreakService,
     {
       provide: 'IPunchClockRepositoryPort',
       useClass: PunchClockRepository,
+    },
+    {
+      provide: 'IBreakRepositoryPort',
+      useClass: BreakRepository,
     },
     {
       provide: APP_INTERCEPTOR,
